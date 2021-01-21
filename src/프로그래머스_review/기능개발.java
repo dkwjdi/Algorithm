c9c6240ba04862c81531801b69d933ff7f6cc9c7
정준영 <dkwjdiq@naver.com>
2021-01-21 22:16:13 +0900

    0121 알고리즘풀이

---
+++ src/프로그래머스_review/기능개발.java
@@ -0,0 +1,48 @@
+package 프로그래머스_review;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 기능개발 {
+	static class Solution{
+		public int[] solution(int []progresses, int[] speeds) {
+			int []days=new int[progresses.length];
+			
+			for(int i=0; i<progresses.length; i++) {
+				int day=1;
+				while(true) { //몇 일 걸리는지
+					if(progresses[i]+day*speeds[i]>=100) break;
+					day++;
+				}
+				days[i]=day;
+			}
+			
+			List<Integer> answerList = new ArrayList<>();
+			int release=0;
+			for(int i=0; i<days.length; i++) {
+				release++;
+				if(i==days.length-1) { //마지막
+					if(days[i-1]>=days[i]) answerList.add(release);
+					else answerList.add(1);
+					break;
+				}
+				if(days[i]<days[i+1]) {
+					answerList.add(release);
+					release=0;
+				}
+			}
+			
+			
+			int [] answer=new int[answerList.size()];
+			for(int i=0; i<answerList.size(); i++) answer[i]=answerList.get(i);
+			return answer;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int []progresses= {93, 30, 55};
+		 int[] speeds= {1, 30, 5};
+		System.out.println(new Solution().solution(progresses, speeds));
+	}
+
+}
